cb711f0ea4971be15780852a595f31496ecbbcff
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-03 01:50:28 +0900

    timezone & introduce Bowerfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
 
   def flash_messages
     markup do |m|
-      m.div(flash[:alert]) if flash[:alert].present?
-      m.div(flash[:notice]) if flash[:notice].present?
+      m.div(flash[:alert], class: 'alert alert-danger', role: 'alert') if flash[:alert].present?
+      m.div(flash[:notice], class: 'alert alert-info', role: 'alert') if flash[:notice].present?
     end
   end
 end
