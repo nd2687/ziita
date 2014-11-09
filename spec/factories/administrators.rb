5802f3d1f9911c83baa91e041d903d05a146c098
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-09 23:06:13 +0900

    add table articles

--- spec/factories/administrators.rb
+++ spec/factories/administrators.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: administrators
+#
+#  id              :integer          not null, primary key
+#  login_name      :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 FactoryGirl.define do
   factory :administrator do
     login_name 'test_user'
