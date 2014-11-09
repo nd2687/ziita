9ce6afaef91e3fb8bbdcf85f811d3a9266e50848
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-10 00:31:49 +0900

    add table comments

---
+++ test/factories/comments.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  article_id :integer
+#  name       :string(255)
+#  body       :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_comments_on_article_id  (article_id)
+#
+
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :comment do
+  end
+end
