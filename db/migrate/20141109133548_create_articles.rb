018a89cb94637ac637a098d1ce510efb2369ca05
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-16 02:06:08 +0900

    db: reference article -> account

--- db/migrate/20141109133548_create_articles.rb
+++ db/migrate/20141109133548_create_articles.rb
@@ -1,10 +1,13 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+      t.references :account, null: false
       t.string :title, null: false
       t.text :body, null: false
 
       t.timestamps
     end
+
+    add_index :articles, :account_id
   end
 end
--- db/seeds/development/articles.rb
+++ db/seeds/development/articles.rb
@@ -1,5 +1,6 @@
 30.times do |i|
   article = Article.new(
+    account_id: 1,
     title: "Ruby 〜〜について その ( #{i+1} )",
     body: 'あああ' * 200
   )
