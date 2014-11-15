531ca8c52611840de4564d1aa018dfe7303c1d78
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-16 02:46:45 +0900

    public page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'top#index'
   resource :session, only: [ :new, :create, :destroy ]
+  resources :articles, only: [ :index, :show ] do
+    resources :comments
+  end
 
   namespace :admin do
     root 'top#index'
