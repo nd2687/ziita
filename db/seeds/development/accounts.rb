0ab9d5c54798ad80e1f0eb26c1e355fc8d1cd0df
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-16 02:46:45 +0900

    public page

--- db/seeds/development/accounts.rb
+++ db/seeds/development/accounts.rb
@@ -1,12 +1,16 @@
-account = Account.new(
-  identify_name: 'totoman',
-  email: 'totoman@example.com',
-  email_for_index: 'totoman@example.com',
-  password: 'password',
-  self_introduction: 'RailsとRubyの勉強を頑張っています。よろしくです!',
-  sites: 'http://nicovideo.jp',
-  company: 'rubyquitous.Inc',
-  residence: 'Yokohama',
-)
-account.setting_password = true
-account.save!
+name = %w(taro hanako hiroyuki takeshi nobuo takahumi kazuma)
+
+name.each do |name|
+  account = Account.new(
+    identify_name: name,
+    email: "#{name}@example.com",
+    email_for_index: "#{name}@example.com",
+    password: 'password',
+    self_introduction: "RailsとRubyの勉強を頑張っています。#{name}と言います。よろしくです!",
+    sites: 'http://hogehoge.jp',
+    company: 'Hoge.Inc',
+    residence: 'Yokohama',
+  )
+  account.setting_password = true
+  account.save!
+end
--- db/seeds/development/articles.rb
+++ db/seeds/development/articles.rb
@@ -1,8 +1,9 @@
-30.times do |i|
+50.times do |i|
+  account_id = Random.rand(1 .. 8)
   article = Article.new(
-    account_id: 1,
+    account_id: account_id,
     title: "Ruby 〜〜について その ( #{i+1} )",
     body: 'あああ' * 200
   )
-  article.save
+  article.save!
 end
