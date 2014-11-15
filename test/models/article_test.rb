0fd696a470b7cbd9904c44c4252a1d44d4980ed9
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-16 02:06:08 +0900

    db: reference article -> account

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,10 +3,15 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
+#  account_id :integer          not null
 #  title      :string(255)      not null
 #  body       :text             not null
 #  created_at :datetime
 #  updated_at :datetime
+#
+# Indexes
+#
+#  index_articles_on_account_id  (account_id)
 #
 
 require 'test_helper'
