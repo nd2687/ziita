13a66c7a27bf8f8263bec134a8a066d2d4d881d0
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-16 19:26:58 +0900

    add actual_user method

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -15,7 +15,7 @@ class User::CommentsController < User::Base
     @comment.name = "名無し" if @comment.name.empty?
     if @comment.save
       flash.notice = "コメントを追加しました！"
-      redirect_to user_article_path(identify_name: current_user.identify_name, id: @article.id)
+      redirect_to user_article_path(identify_name: actual_user.identify_name, id: @article.id)
     else
       flash.now[:alert] = "コメントの追加に失敗しました。"
       render action: 'new'
@@ -30,6 +30,6 @@ class User::CommentsController < User::Base
   end
 
   def prepare_article
-    @article = current_user.articles.find(params[:article_id])
+    @article = actual_user.articles.find(params[:article_id])
   end
 end
