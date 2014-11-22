cdda30497b35d4d8b0d802f95cf72fbcb0317880
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-23 02:40:06 +0900

    another tab & fix coderay css

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,8 @@ lock '3.2.1'
 
 set :application, 'ziita'
 set :repo_url, 'git@bitbucket.org:totoman/ziita.git'
-set :log_level, :info
+set :log_level, :debug
+set :keep_releases, 3
 set :linked_files, %w{ config/database.yml config/secrets.yml }
 set :linked_dirs, %w{
   log tmp/pids tmp/cache tmp/sockets config/unicorn
