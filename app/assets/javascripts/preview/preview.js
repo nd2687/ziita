cadb50527789000f369d92fb37d33774dfd31532
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-22 19:18:20 +0900

    js: scroll_top

--- app/assets/javascripts/preview/preview.js
+++ app/assets/javascripts/preview/preview.js
@@ -1,17 +1,18 @@
 $(function() {
-  marked.setOptions({
-      langPrefix: ''
-  });
-
-  $('#article_body').keyup(function() {
-    var src = $(this).val();
-    var html = marked(src);
-
-    $('#result').html(html);
-
-    $('pre code').each(function(i, block) {
-        hljs.highlightBlock(block);
+  if($('#preview-page').length){
+    marked.setOptions({
+        langPrefix: ''
     });
-  });
-});
 
+    $('#article_body').keyup(function() {
+      var src = $(this).val();
+      var html = marked(src);
+
+      $('#result').html(html);
+
+      $('pre code').each(function(i, block) {
+          hljs.highlightBlock(block);
+      });
+    });
+  };
+});
