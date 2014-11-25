84856d7f1e65f455c944efdef590dd59fa93ca32
Takashi Kawano <kawano.fanta@gmail.com>
2014-11-26 07:21:40 +0900

    fix any css

--- app/assets/javascripts/user/account.js
+++ app/assets/javascripts/user/account.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on("ready page:load", function () {
   $('input[id=lefile]').change(function() {
     $('#photoCover').val($(this).val());
   });
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @tags = Article.tag_counts.map{|t|t.name}
   end
 end
