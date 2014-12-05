b1e90c6586bbfcc99401342fb9cc32239cf296c3
Takashi Kawano <kawano.fanta@gmail.com>
2014-12-06 03:38:43 +0900

    add noimage

--- app/assets/javascripts/preview/preview.js
+++ app/assets/javascripts/preview/preview.js
@@ -18,8 +18,12 @@ $(function() {
     var preview = $('#result');
     var input = $('#article_body');
 
-    preview.scroll(function(){
-      input.scrollTop(preview.scrollTop());
+    input.scroll(function(){
+      var change_height_pre = preview.get(0).scrollHeight;
+      var change_height = input.get(0).scrollHeight;
+      var input_percentage = input.scrollTop() / change_height;
+      var pre_top = change_height_pre * input_percentage;
+      preview.scrollTop(pre_top);
     });
   };
 });
