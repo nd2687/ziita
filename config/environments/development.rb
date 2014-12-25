a78ce7c8ba76e42c7febe76377743e7c2ece011c
Takashi Kawano <kawano.fanta@gmail.com>
2014-12-25 22:07:07 +0900

    update rails 4.2.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.assets.digest = true
+
   # bullet
   config.after_initialize do
     Bullet.enable = true
